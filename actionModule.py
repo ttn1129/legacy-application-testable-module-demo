1a607c32264c5a78eacde34eb69142e27a27ff42
ttn1129 <ttnsky1129@yahoo.co.jp>
2019-09-03 21:44:44 +0900

    Update actionModule.py

--- actionModule.py
+++ actionModule.py
@@ -1,8 +1,7 @@
 # coding:utf-8
-# from selenium import webdriver
 import config
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.action_chains import ActionChains,Command
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -11,15 +10,24 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support.select import Select
 from PIL import ImageGrab
-import time
 from OsModule import OsModule
 osModule = OsModule()
-# from models.FamilyData import FamilyData
 
-# from datetime import datetime
 screenshotDirectoryName = config.screenshotDirectoryName
-screenshotBasePath = config.screenshotDirectoryName
+screenshotBasePath = config.screenshotBasePath
 
+def scrollToElement(driver: WebDriver, element:WebElement)->None:
+    actions = ActionChains(driver)
+    actions.move_to_element(element).perform()
+    return
+
+def screenshotWithElementNameSearch(driver: WebDriver, name:str, filename: str = 'screenshotOfDisplay.png')->None:
+    scrollToElement(
+        driver,
+        driver.find_element_by_name(name)
+    )
+    screenshotOfDisplay(driver, filename)
+    return
 
 def screenshotOfDisplay(driver: WebDriver, filename: str = 'screenshotOfDisplay.png'):
     osModule.makeDirectoryIfNotExist(screenshotDirectoryName)
@@ -34,8 +42,16 @@ def clickElement(driver:WebDriver, element:WebElement)->None:
     actionsClickElement.click()
     actionsClickElement.perform()
 
+def clickElementWithFrameNameAndXPath(driver:WebDriver, framename:str, xpathToElement:str)->None:
+    driver.switch_to_frame(driver.find_element_by_xpath('//frame[@name="'+ framename +'"]'))
+    clickElement(
+        driver,
+        driver.find_element_by_xpath(xpathToElement)
+    )
+    WebDriverWait(driver, 15).until(EC.presence_of_all_elements_located)
+    driver.switch_to.default_content()
+
 def getValueFrom(element:WebElement):
-    # return element.get_attribute('value').encode('utf-8')
     return element.get_attribute('value')
 
 def getValueFromElementByNameSearch(targetElement:WebElement, searchName:str):
@@ -84,5 +100,37 @@ def selectOptionValue(targetSelectElement:WebElement, selectValue)->None:
     Select(targetSelectElement).select_by_value(selectValue)
     return
 
+def openAndMoveToNewTab(driver:WebDriver, url:str="about:blank"):
+    driver.execute_script("window.open('"+url+"')")
+    return
+
+def setValueWithFrameNameAndXPath(driver:WebDriver, framename:str, xpathToElement:str, value)->None:
+    driver.switch_to_frame(driver.find_element_by_xpath('//frame[@name="'+ framename +'"]'))
+    setValueTo(
+        driver.find_element_by_xpath(xpathToElement),
+        value
+    )
+    driver.switch_to.default_content()
+    return
+
+def getValueInFrameByFrameNameAndXPath(driver:WebDriver, framename:str, xpathToElement:str):
+    driver.switch_to_frame(driver.find_element_by_xpath('//frame[@name="'+ framename +'"]'))
+    val = getValueFrom(
+        driver.find_element_by_xpath(xpathToElement)
+    )
+    driver.switch_to.default_content()
+    return val
+
+def getTextWithId(driver:WebDriver, idName:str):
+    return getText(
+        driver.find_element_by_id(idName)
+    )
+
+def getTextWithXPathSearch(driver:WebDriver, xpath:str):
+    print(xpath)
+    return getText(
+        driver.find_element_by_xpath(xpath)
+    )
 def getText(element:WebElement)->str:
+    print(element.text)
     return element.text
